4424b0704a5a6b1fd122dd9c428ef03a519f102a
Eric Jackson <eric@deepweave.com>
2017-07-03 14:59:57 -0400

    Just saving state.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions allows caching of a potentially expensive matrix inversion.
+##
+## The makeCacheMatrix routine creates a special version of a matrix that caches
+## the result of a matrix inversion (although there is nothing in the makeCacheMatrix
+## routine specific to matrix inversion since the routine using it chooses what to set
+## the invMatrix value to). 
+## routine makes use ...
+##
+## The cacheSolve routine returns the inverse of the input matrix. If the matrix has
+## not yet been encountered, it will compute the inverse and cache it. On subsequent calls
+## with the same input matrix, cacheSolve will simple return the cached version. It only
+## caches the result for the most recent input matrix.
 
-## Write a short comment describing this function
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        invMatrix <- NULL
+        set <- function(y) {
+                x <<- y
+                invMatrix <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inv) invMatrix <<- inv
+        getinverse <- function() invMatrix
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
@@ -12,4 +30,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        mi <- x$getinverse()
+        if (!is.null(mi)) {
+                message("getting the cached version of the matrix inverse")
+                return(mi)
+        }
+        data <- x$get()
+        mi <- solve(data, ...)
+        x$setinverse(mi)
+        mi
 }
